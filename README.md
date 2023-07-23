c17a3a44371683a1f7d1ffe496ab5a25b08c69b4
Ramakanth <ramakanthvattipally@gmail.com>
2023-07-23 16:27:32 +0100

    commit changes to backend

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# backenddeploy2307
+# dotnet-app-service
+This repository is to learn how to automate deployment to Azure App Service using Terraform.
---
+++ terraform/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-resource-group"
+    storage_account_name = "ramterraformstate2307"
+    container_name       = "terraform-state-container"
+    key                  = "terraform.backendstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
